d10ed05633e5fe1362f79a53a35441d6435bcce2
ClaraEd <167836761+ClaraEd@users.noreply.github.com>
2024-04-24 20:41:53 -0400

    Add files via upload

---
+++ tipCal.js
@@ -0,0 +1,39 @@
+// document.querySelector("#tip-form").onchange =
+// function () {
+
+//   var bill = Number(document.getElementById("billTotal").value);
+//   var tip = document.getElementById("tipInput").value;
+//   document.getElementById("tipOutput").innerHTML = `${tip}%`;
+//   var tipValue = bill * (tip / 100);
+//   var finalBill = bill + tipValue;
+//   console.log(finalBill);
+//   var tipAmount = document.querySelector("#tipAmount");
+//   var totalBillWithTip = document.querySelector("#totalBillWithTip");
+
+//   tipAmount.value = tipValue.toFixed(2);
+//   totalBillWithTip.value = finalBill.toFixed(2);
+
+//   document.getElementById("result").style.display = "block";
+// };
+
+document.getElementById("result").style.display = "none";
+
+document.querySelector("#tip-form").onchange = function () {
+  var bill = Number(document.getElementById("billTotal").value);
+  var tip = document.getElementById("tipInput").value;
+  document.getElementById("tipOutput").innerHTML = `${tip}%`;
+
+  var tipValue = bill * (tip / 100);
+  var finalBill = bill + tipValue;
+  console.log(finalBill);
+
+  var tipAmount = document.querySelector("#tipAmount");
+  var totalBillWithTip = document.querySelector("#totalBillWithTip");
+
+  tipAmount.value = tipValue.toFixed(2);
+  totalBillWithTip.value = finalBill.toFixed(2);
+
+  //Show Results
+
+  document.getElementById("result").style.display = "block";
+};
